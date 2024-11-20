f2cd02216ed3e498a7969fc65805f058dae36659
AnnaParysheva <parysevaanna36@gmail.com>
2024-11-20 18:22:37 +0300

    Добавьте файлы проекта.

---
+++ Files_Labs/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace Files_Labs
+{
+    enum Days_of_week
+    {
+        Понедельник=1,
+        Вторник=2,
+        Среда=3,
+        Четверг=4,
+        Пятница=5,
+        Суббота=6,
+        Воскресенье=7
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Дана последовательность из 10 чисел. Определить, является ли эта последовательность упорядоченной по возрастанию.В случае отрицательного ответа определить порядковый номер первого числа, которое нарушает данную последовательность. Использовать break.
+            Console.WriteLine("1. Дана последовательность из 10 чисел. Определить, является ли эта последовательность упорядоченной по возрастанию.");
+            int[] numbers = new int[10];
+            int j = 0;
+            Console.WriteLine("Введите 10 чисел:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Число N{i + 1}: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int index_mistakes = -1;
+            for (j = 0; j < numbers.Length - 1; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    index_mistakes = j + 2;
+                    break;
+                }
+            }
+            if (index_mistakes == -1)
+            {
+                Console.WriteLine("Последовательность упорядочена по возрастанию.");
+            }
+            else
+            {
+                Console.WriteLine($"Последовательность нарушена на позиции {index_mistakes}");
+            }
+            //2. Игральным картам условно присвоены следующие порядковые номера в зависимости от их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14. Порядковые номера остальных карт соответствуют их названиям(«шестерка», «девятка» и т.п.).По заданному номеру карты k(6 <= k <= 14) определить достоинство соответствующей карты. Использовать try-catch-finally.
+            Console.WriteLine("2. По заданному номеру карты k(6 <= k <= 14) определить достоинство соответствующей карты.");
+            try
+            {
+                Console.Write("Введите номер карты (от 6 до 14): ");
+                int k = int.Parse(Console.ReadLine());
+                if (k < 6 || k > 14)
+                {
+                    throw new ArgumentOutOfRangeException("Номер карты должен быть в диапазоне от 6 до 14.");
+                }
+                string value;
+                switch (k)
+                {
+                    case 6:
+                        value = "шестёрка";
+                        break;
+                    case 7:
+                        value = "семёрка";
+                        break;
+                    case 8:
+                        value = "восьмёрка";
+                        break;
+                    case 9:
+                        value = "девятка";
+                        break;
+                    case 10:
+                        value = "десятка";
+                        break;
+                    case 11:
+                        value = "валет";
+                        break;
+                    case 12:
+                        value = "дама";
+                        break;
+                    case 13:
+                        value = "король";
+                        break;
+                    case 14:
+                        value = "туз";
+                        break;
+                    default:
+                        value = null; 
+                        break;
+                }
+
+                Console.WriteLine($"Достоинство карты: {value}");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Конец программы");
+            }
+            // 3. Напишите программу, которая принимает на входе строку и производит выходные данные в соответствии со следующей таблицей:
+            /* Примечание. По умолчанию используется “все остальное”: если входные данные
+            функции не соответствуют ни одному из значений в таблице, то возвращаемое значение
+            должно быть «Beer».
+            Убедитесь, что вы охватили случаи, когда некоторые слова не отображаются с
+            правильным написанием заглавных букв.Например, ввод «pOLitiCIaN» должен
+            по - прежнему возвращать «Your tax dollars». */
+            Console.WriteLine("3. Напишите программу, которая принимает на входе строку и производит выходные данные в соответствии со следующей таблицей");
+            Console.WriteLine("Введите строку:");
+            string stroka= Console.ReadLine().ToLower();
+            string result;
+            switch (stroka)
+                {
+                    case "jabroni":
+                        result = "Patron Tequila";
+                        break;
+                    case "school counselor":
+                        result = "Anything with Alcohol";
+                        break;
+                    case "programmer":
+                        result = "Hipster Craft Beer";
+                        break;
+                    case "bike gang member":
+                        result = "Moonshine";
+                        break;
+                    case "politician":
+                        result = "Your tax dollars";
+                        break;
+                    case "rapper":
+                        result = "Cristal";
+                        break;
+                    default:
+                        result="Beer";
+                        break;
+                }
+
+                Console.WriteLine(result);
+            //4. Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,...,7) выводит на экран его название(понедельник, вторник, ..., воскресенье). Использовать enum.
+            Console.WriteLine("4. Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,...,7) выводит на экран его название (понедельник, вторник, ..., воскресенье).");
+            Console.Write("Введите номер дня недели: ");
+            byte number_day = byte.Parse(Console.ReadLine());
+            if (number_day >= 1 && number_day <= 7)
+            {
+                Days_of_week day = (Days_of_week)number_day;
+                Console.WriteLine($"День недели: {day}");
+            }
+            else
+            {
+                Console.WriteLine("Пожалуйста, введите число от 1 до 7.");
+            }
+            //5. Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента "Hello Kitty" или "Barbie doll" необходимо положить их в “сумку”, т.е.прибавить к результату.Вывести на экран сколько кукол в “сумке”.
+            Console.WriteLine("5. Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента Hello Kitty или Barbie doll необходимо положить их в “сумку”, т.е. прибавить к результату. Вывести на экран сколько кукол в “сумке”.");
+            string[] toys = { "Hello Kitty", "Barbie doll", "Plush bear", "Toy car", "Barbie doll", "Dollhouse", "Hello Kitty" };
+            int count = 0;
+            foreach (string toy in toys)
+            {
+                if (toy == "Hello Kitty" || toy == "Barbie doll")
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество кукол в “сумке”: {count}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tumakov_Labs/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Tumakov_Labs
+{
+    enum Month
+    {
+        января=1,
+        февраля=2,
+        марта=3,
+        апреля=4,
+        мая=5,
+        июня=6,
+        июля=7,
+        августа=8,
+        сентября=9,
+        октября=10,
+        ноября=11,
+        декабря=12
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Упражнение 4.1 Написать программу, которая читает с экрана число от 1 до 365 (номер дня в году), переводит этот число в месяц и день месяца.Например, число 40 соответствует 9 февраля(високосный год не учитывать).
+            Console.WriteLine("Задание 4.1 Написать программу, которая читает с экрана число от 1 до 365 (номер дня в году), переводит этот число в месяц и день месяца.");
+            Console.Write("Введите номер дня в году (от 1 до 365): ");
+            int day1 = 0;
+            int i1 = 0;
+            int number_day1 = int.Parse(Console.ReadLine());
+            int[] days_in_month1 = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            for (i1 = 0; i1 < days_in_month1.Length; i1++)
+            {
+                if (number_day1 <= days_in_month1[i1])
+                {
+                    day1 = number_day1;
+                    break;
+                }
+                number_day1 -= days_in_month1[i1];
+            }
+            Month month1 = (Month)i1 + 1;
+            Console.WriteLine($"{day1} {month1}");
+            // Упражнение 4.2 Добавить к задаче из предыдущего упражнения проверку числа введенного пользователем.Если число меньше 1 или больше 365, программа должна вырабатывать исключение, и выдавать на экран сообщение.
+            Console.WriteLine("Упражнение 4.2 Добавить к задаче из предыдущего упражнения проверку числа введенного пользователем.");
+            try
+            {
+                Console.Write("Введите номер дня в году (от 1 до 365): ");
+                int day2 = 0;
+                int i2 = 0;
+                int number_day2 = int.Parse(Console.ReadLine());
+                if (number_day2 < 1 || number_day2 > 365)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                int[] days_in_month2 = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                for (i2 = 0; i2 < days_in_month2.Length; i2++)
+                {
+                    if (number_day2 <= days_in_month2[i2])
+                    {
+                        day2 = number_day2;
+                        break;
+                    }
+                    number_day2 -= days_in_month2[i2];
+                }
+                Month month2 = (Month)i2 + 1;
+                Console.WriteLine($"{day2} {month2}");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Номер дня должен быть в диапазоне от 1 до 365.");
+            }
+            // Домашнее задание 4.1 Изменить программу из упражнений 4.1 и 4.2 так, чтобы она учитывала год(високосный или нет). Год вводится с экрана. (Год високосный, если он делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный год.Однако, если он делится без остатка на 400, это високосный год.)
+            Console.WriteLine("Домашнее задание 4.1 Изменить программу из упражнений 4.1 и 4.2 так, чтобы она учитывала год(високосный или нет).");
+            try
+            {
+                Console.Write("Введите год: ");
+                int year = int.Parse(Console.ReadLine());
+                bool visokos = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+                int max_day = visokos ? 366 : 365;
+                Console.Write($"Введите номер дня в году (от 1 до {max_day}): ");
+                int day = 0;
+                int i = 0;
+                int number_day = int.Parse(Console.ReadLine());
+                if (number_day < 1 || number_day > max_day)
+                {
+                    throw new ArgumentOutOfRangeException($"Номер дня должен быть в диапазоне от 1 до {max_day}.");
+                }
+                int[] days_in_month = { 31, visokos ? 29: 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                for (i = 0; i < days_in_month.Length; i++)
+                {
+                    if (number_day <= days_in_month[i])
+                    {
+                        day = number_day;
+                        break;
+                    }
+                    number_day -= days_in_month[i];
+                }
+                Month month = (Month)i + 1;
+                Console.WriteLine($"{day} {month} {year}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
